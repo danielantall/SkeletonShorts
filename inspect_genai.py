316a50305f1198091fd4be8c77dc38891fe82a7c
Daniel Antal <danielantal566@gmail.com>
2026-04-12 14:19:39 -0400

    Cleanup

--- inspect_genai.py
+++
@@ -1,6 +0,0 @@
-import os
-from google import genai
-from google.genai import types
-
-print(dir(genai.Client))
-print(dir(genai.models.Models))
--- inspect_genai_video.py
+++
@@ -1,7 +0,0 @@
-import inspect
-from google import genai
-
-try:
-    print("aio:", dir(genai.Client().aio.models))
-except Exception as e:
-    print(e)
